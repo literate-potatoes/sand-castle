30752ee917a33ea3e30cf26b7e11691e2aa5e2f9
Kamal Nasser <hello@kamal.io>
2022-02-12 00:11:38 +0200

    test

--- packages/contacts/list/index.js
+++ packages/contacts/list/index.js
@@ -1,28 +1,10 @@
-exports.main = (args) => {
-    // const { Client } = require('pg')
-    // const client = new Client({
-    //     connectionString: (process.env.DATABASE_URL || "").replace("?sslmode=require", ""),
-    //     ssl: {
-    //         rejectUnauthorized: false,
-    //         require: true,
-    //     }
-    // })
-    return (async () => {
-        // await client.connect()
-        // const res = await client.query('select * from contacts')
-        // await client.end()
-        // const contacts = res.rows
-
-        return {
-            headers: {
-                'content-type': 'application/json',
-            },
-            body: {
-                message: "'ello there!",
-                // contacts,
-            },
-        }
-    })()
+exports.main = args => {
+    return {
+        headers: {
+            'content-type': 'application/json',
+        },
+        body: {
+            message: "'ello there!"
+        },
+    }
 }
-
-// console.log(exports.main())

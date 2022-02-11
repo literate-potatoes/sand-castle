214d6f0a28870c46d4fffec046edbe7df7e727db
Kamal Nasser <hello@kamal.io>
2022-02-11 21:43:11 +0200

    db access

--- packages/contacts/list/index.js
+++ packages/contacts/list/index.js
@@ -1,10 +1,20 @@
-exports.main = args => {
+exports.main = async (args) => {
+    const { Client } = require('pg')
+    const client = new Client({
+        connectionString: process.env.DATABASE_URL,
+    })
+    await client.connect()
+    const res = await client.query('SELECT * from contacts')
+    await client.end()
+    const contacts = res.rows
+
     return {
         headers: {
             'content-type': 'application/json',
         },
         body: {
-            message: "'ello there!"
+            message: "'ello there!",
+            contacts,
         },
     }
 }

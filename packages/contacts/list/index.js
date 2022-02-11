5dcd1d080beb57fbe60ae38a2e3b2dfbac6b8059
Kamal Nasser <hello@kamal.io>
2022-02-11 22:07:27 +0200

    test

--- packages/contacts/list/index.js
+++ packages/contacts/list/index.js
@@ -1,20 +1,28 @@
-exports.main = async (args) => {
+exports.main = (args) => {
     const { Client } = require('pg')
     const client = new Client({
-        connectionString: process.env.DATABASE_URL,
+        connectionString: (process.env.DATABASE_URL || "").replace("?sslmode=require", ""),
+        ssl: {
+            rejectUnauthorized: false,
+            require: true,
+        }
     })
-    await client.connect()
-    const res = await client.query('SELECT * from contacts')
-    await client.end()
-    const contacts = res.rows
+    return (async () => {
+        await client.connect()
+        const res = await client.query('select * from contacts')
+        await client.end()
+        const contacts = res.rows
 
-    return {
-        headers: {
-            'content-type': 'application/json',
-        },
-        body: {
-            message: "'ello there!",
-            contacts,
-        },
-    }
+        return {
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: {
+                message: "'ello there!",
+                contacts,
+            },
+        }
+    })()
 }
+
+// console.log(exports.main())
